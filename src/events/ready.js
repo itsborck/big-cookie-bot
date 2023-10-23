9de3077f88d01add3411cb68693db2edd95681bd
Brock Pavelko <bpavelko624@gmail.com>
2023-10-23 14:52:29 -0400

    this should add voice support?

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,14 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setActivity({
+            name: 'bigcookie.org',
+            type: ActivityType.Playing,
+            url:'bigcookie.org'
+        });
     },
 };
