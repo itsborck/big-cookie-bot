2e7107200e226ce42c23f79df819ec54dad49bf3
Brock Pavelko <bpavelko624@gmail.com>
2023-10-23 18:01:18 -0400

    moderation commands

---
+++ src/commands/moderation/ban.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Bans the specified user.")
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The member you'd like to ban.")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("reason")
+        .setDescription("The reason for banning the user.")
+    ),
+  async execute(interaction, client) {
+    const user = interaction.options.getUser("target");
+    const member = await interaction.guild.members
+      .fetch(user.id)
+      .catch(console.error);
+
+    if (!reason) reason = "No reason provided.";
+
+    user
+      .send({
+        content: `You have been banned from **${interaction.guild.name}** for **${reason}**.`,
+      })
+      .catch(console.log("user has DMs disabled."));
+
+    await member
+      .ban({
+        deleteMessageDays: 1,
+        reason: reason,
+      })
+      .catch(console.error);
+
+    await interaction.reply({
+      content: `Successfully banned **${user.tag}**.`,
+    });
+  },
+};
